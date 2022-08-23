42c9a562c5dd637cf89e57d724a981b5782e472e
Yan Xu <xudaiyanzi@gmail.com>
2022-08-23 16:47:42 -0500

    fix the color

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -22,7 +22,6 @@ const Home = () => {
                 <div className='text_div'>
                     <h4>
                         {'Hi, my name is Yan.'}
-                        {'testColor'}
                     </h4>
                     <h2>
                         {'I am a '}
