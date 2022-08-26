3a84497cda8a27258d6f06a9198a348a05b87e23
Yan Xu <xudaiyanzi@gmail.com>
2022-08-26 16:15:09 -0500

    center the project-image2

--- src/Component/Projects/Card.js
+++ src/Component/Projects/Card.js
@@ -11,17 +11,19 @@ const Card = (props) => {
                         return (
                             <article className='projects_item' key={index}>
                                 <h6>{val.category}</h6>
-                                <div className='projects_item_img'>
-                                    <img src={val.image} alt=''/>
-                                </div>
-                                <div>
-                                    <a href={val.image_site} className='pic_copyright' target='_blank'>Picture copyRight</a>
-                                </div>
-                                <div className='projects_item_title'>
-                                    <h5>{val.title}</h5>
-                                </div>
-                                <div className='projects_item_links'>
-                                    <a href={val.url} className='projects_btn' target='_blank'>Demo</a>
+                                <div className='projects_item_details'>
+                                    <div className='projects_item_img'>
+                                        <img src={val.image} alt=''/>
+                                    </div>
+                                    <div>
+                                        <a href={val.image_site} className='pic_copyright' target='_blank'>Picture copyRight</a>
+                                    </div>
+                                    <div className='projects_item_title'>
+                                        <h5>{val.title}</h5>
+                                    </div>
+                                    <div className='projects_item_links'>
+                                        <a href={val.url} className='projects_btn' target='_blank'>Demo</a>
+                                    </div>
                                 </div>
                             </article>
                         )
@@ -29,14 +31,16 @@ const Card = (props) => {
                         return (
                             <article className='projects_item' key={index}>
                                 <h6>{val.category}</h6>
-                                <div className='projects_item_img'>
-                                    <img src={val.image} alt=''/>
-                                </div>
-                                <div className='projects_item_title'>
-                                    <h5>{val.title}</h5>
-                                </div>
-                                <div className='projects_item_links'>
-                                    <a href={val.url} className='projects_btn' target='_blank'>Demo</a>
+                                <div className='projects_item_details'>
+                                    <div className='projects_item_img'>
+                                        <img src={val.image} alt=''/>
+                                    </div>
+                                    <div className='projects_item_title'>
+                                        <h5>{val.title}</h5>
+                                    </div>
+                                    <div className='projects_item_links'>
+                                        <a href={val.url} className='projects_btn' target='_blank'>Demo</a>
+                                    </div>
                                 </div>
                             </article>
                         )
